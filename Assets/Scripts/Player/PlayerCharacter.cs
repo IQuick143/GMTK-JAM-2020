39d222ba74ffb618aa5d647e434262a2526b59f3
IQuick 143 <IQuick143cz@gmail.com>
2020-07-11 01:02:45 +0200

    Made the player move a bit xd

---
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour {
+	[SerializeField]
+	private PlayerMove move;
+	// Start is called before the first frame update
+	void Start() {
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (move.MoveVector.sqrMagnitude > 0.0001) {
+			this.transform.rotation = Quaternion.LookRotation(Vector3.Lerp(this.transform.forward, move.MoveVector, 0.2f), this.transform.up);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMove : MonoBehaviour {
+	public Vector3 MoveVector = Vector3.zero;
+	private new Rigidbody rigidbody;
+	[SerializeField]
+	private float speed = 2f;
+
+	void Awake() {
+		this.rigidbody = this.GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		MoveVector = Input.GetAxis("Horizontal") * this.transform.right + Input.GetAxis("Vertical") * this.transform.forward;
+		if (MoveVector.sqrMagnitude > 1) MoveVector.Normalize();
+		this.rigidbody.velocity = MoveVector * speed;
+	}
+}
