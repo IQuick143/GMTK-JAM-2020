f9173679057de6531bc141b1c7a4d3c9e8e27e68
IQuick 143 <IQuick143cz@gmail.com>
2020-07-11 12:21:51 +0200

    Added Fire and Extenguishers

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(Collider))]
+public class FireController : MonoBehaviour {
+	[SerializeField]
+	private float fireAmount = 1f;
+	[SerializeField]
+	private float extenguishmentFactor = 0.01f;
+	private ParticleSystem fireSystem;
+
+	// Start is called before the first frame update
+	void Start() {
+		fireSystem = this.GetComponent<ParticleSystem>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		var emission = fireSystem.emission;
+		emission.rateOverTime = 1f + fireAmount;
+		if (fireAmount < 0f) {
+			//Fire extenguished
+		}
+	}
+
+	void OnParticleCollision(GameObject other) {
+		if (other.GetComponent<Extinguisher>() != null) {
+			var part = other.GetComponent<ParticleSystem>();
+			var col = new List<ParticleCollisionEvent>();
+			int num_col = part.GetCollisionEvents(this.gameObject, col);
+			fireAmount -= num_col * extenguishmentFactor;
+		}
+	}
+}
