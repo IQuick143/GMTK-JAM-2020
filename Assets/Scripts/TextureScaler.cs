a2c0b1446dc3d072ec0b37602595082a3cb2294e
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-07-12 17:52:40 +0200

    adding materials

---
+++ Assets/Scripts/TextureScaler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScaler : MonoBehaviour
+{
+    [SerializeField] private float tileX = 1;
+    [SerializeField] private float tileY = 1;
+    
+    private Mesh mesh;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    void Update()
+    {
+        mat.mainTextureScale = new Vector2(
+            (mesh.bounds.size.x * Math.Max(transform.localScale.x, transform.localScale.z)) * tileX,
+            (mesh.bounds.size.y * transform.localScale.y) * tileY);
+    }
+}
