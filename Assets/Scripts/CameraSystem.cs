01fcd8c474f49f4df11fb494782c1d35cf6507c7
27chandler <chandler27s@yahoo.com>
2020-07-12 17:17:42 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    [SerializeField] private List<Camera> cameras = new List<Camera>();
+    [SerializeField] private float camera_time = 8.0f;
+    private float current_time = 0.0f;
+    private int camera_index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current_time += Time.deltaTime;
+
+        if (current_time > camera_time)
+        {
+            current_time = 0.0f;
+
+        }
+    }
+}
