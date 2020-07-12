df1c95e100162d453b4beaecb1e834344621feb8
27chandler <chandler27s@yahoo.com>
2020-07-12 13:45:11 +0100

    Fixed extra bracket

--- Assets/Scripts/Customer/CustomerAI.cs
+++ Assets/Scripts/Customer/CustomerAI.cs
@@ -122,7 +122,6 @@ public class CustomerAI : MonoBehaviour {
             renderer.material.color = new Color(1f, otherColor, otherColor, 1f);;
         }
         Debug.Log("Hunger: " + (hunger / hunger_threshold) * 0.5f);
-    }
 
         if (rigidbody.velocity.magnitude > 1.0f)
         {
