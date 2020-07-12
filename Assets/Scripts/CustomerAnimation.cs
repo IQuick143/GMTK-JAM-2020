d6d9077ca82f8a2a7beef39f0ca9710bcb2bb7a9
JaumeMontagut <jaume.montagut.i.guix@gmail.com>
2020-07-12 12:25:45 +0200

    adding customer with animations
    :smile:

---
+++ Assets/Scripts/CustomerAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+    public float velocityThreshold;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent <Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rb.velocity.magnitude < velocityThreshold)
+        {
+            anim.SetBool("fast", false);
+        }
+        else
+        {
+            anim.SetBool("fast", true);
+        }
+    }
+}
